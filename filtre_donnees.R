9de7cc4cb5284ccb1cec302e97ba00e6cf3c4621
aubinpoissonnier <a.poissonnier@univ-lyon2.fr>
2023-10-06 17:21:38 +0000

    RP

--- filtre_donnees.R
+++ filtre_donnees.R
@@ -1,4 +1,48 @@
 library(aws.s3)
+library(purrr)
+## Données
+
+df <- map(
+  .x = c("/analyse_quanti/FD_INDCVIZE_2019.csv",
+         "/analyse_quanti/FD_INDCVIZE_2017.csv",
+         "/analyse_quanti/FD_INDCVIZE_2015.txt",
+         "/analyse_quanti/FD_INDCVIZD_2013.txt",
+         "/analyse_quanti/FD_indcvizd_2011.txt",
+         "/analyse_quanti/FD_INDCVIZD_2009.txt"),
+  .f =
+    ~ aws.s3::s3read_using(
+      FUN = data.table::fread,
+      # Mettre les options de FUN ici
+      object = .,
+      bucket = "aubinpoissonnier",
+      opts = list("region" = ""),
+      select =
+        c(ARM="character",
+             IPONDI="character",
+             IMMI="factor",
+             SEXE="factor")
+))
+
+df2 <- df %>%
+  list_rbind(
+    names_to = "id"
+  )
+
+df2 <- df2[ARM %in% 69000:70000]
+
+aws.s3::s3write_using(
+  df2,
+  FUN = data.table::fwrite,
+  object = "diffusion/RP_LYON_2009_2019.csv",
+  bucket = "aubinpoissonnier",
+  opts = list("region" = "")
+)
+
+
+
+
+
+
 
 aws.s3::get_bucket("aubinpoissonnier",
                    prefix = "diffusion/",
